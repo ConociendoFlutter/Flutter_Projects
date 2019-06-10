820f2f063e58475c0028c3e2f80106393d18fbb2
Coniciendo Flutter <conociendoflutter@gmail.com>
2019-06-10 17:43:09 +0200

    Add Tipos de datos Enteros-String

---
+++ Introducción_Dart/tipos-datos-enteros-string.dart
@@ -0,0 +1,15 @@
+void main() {
+// Números enteros
+  int empleados = 10;
+
+// Números dobles
+  double pi = 3.141592;
+
+  print('$empleados - $pi');
+
+  // String - Cadena de caracteres
+  String nombre = 'Conociendo Flutter';
+  print(nombre);
+  print(nombre[0]);
+  print(nombre[nombre.length - 1]);
+}
