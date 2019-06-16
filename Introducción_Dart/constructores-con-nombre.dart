6259251696c02f79a06175c20831e1e944b93fb0
Coniciendo Flutter <conociendoflutter@gmail.com>
2019-06-16 17:03:34 +0200

    Add constructores con nombres en Dart.

--- Introducción_Dart/constructores-con-nombre.dart
+++ Introducción_Dart/constructores-con-nombre.dart
@@ -1,1 +1,28 @@
+import 'dart:convert';
 
+void main() {
+  // final wolverine = Heroe('Logan', 'Regeneración');
+// Creamos un JSON.
+  final rawJson = '{"nombre":"Logan", "poder": "Regeneración"}';
+  Map parsedJson = json.decode(rawJson);
+
+  final wolverine = Heroe.fromJson(parsedJson);
+
+  //print(parsedJson);
+
+  print(wolverine.nombre);
+  print(wolverine.poder);
+}
+
+class Heroe {
+  String nombre;
+  String poder;
+
+  Heroe(this.nombre, this.poder);
+
+// Constructor con nombre:
+  Heroe.fromJson(Map parsedJson) {
+    nombre = parsedJson["nombre"];
+    poder = parsedJson["poder"];
+  }
+}
